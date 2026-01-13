b4dfe476ecd63b3519bdef3295e7920c744290fd
BhagyashreeNBijap <bhagyan125@gmail.com>
2026-01-13 18:15:06 +0530

    Add built-in math functions demonstration

---
+++ Day2_class/Tc_BuiltInFunction.py
@@ -0,0 +1,9 @@
+import math
+print(math.sqrt(64))
+print(math.log(100))
+print(math.gamma(100))
+print(math.pow(2,10))
+print(math.sin(math.pi))
+print(math.factorial(4))
+print(math.ceil(math.pi))
+print(math.floor(math.pi))
