d074c0622735307f36af438e7ddb81da7fdf9712
BhagyashreeNBijap <bhagyan125@gmail.com>
2026-01-13 18:20:34 +0530

    Create TC_String.py with string operations
    Add string manipulation examples in TC_String.py

---
+++ Day2_class/TC_String.py
@@ -0,0 +1,32 @@
+a=("Hello")
+print(a[0])
+print(a[-5])
+print(a)
+
+b="""Hello
+welcome
+to
+python"""
+print(b)
+
+text="Python"
+print(text[0:3])
+print(text[2:])
+print(text[:3])
+print(a+text)
+
+print(a+text)
+print("Hi" *3)
+
+print(text.upper())
+print(a.replace("python","java"))
+
+print(len(a))
+
+print("a" in "apple")
+print("a" not  in "apple")
+s1="I am {0} ans I am {1} old".format("hema",30)
+print(s1)
+
+s2=b.split()
+print(s2)
