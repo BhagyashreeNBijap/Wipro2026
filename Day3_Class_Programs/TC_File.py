e7cbfcbd482f61a2df19a2b303b9750232e00225
BhagyashreeNBijap <bhagyan125@gmail.com>
2026-01-14 23:40:48 +0530

    Implement file read and write operations
    Read from and write to 'f1.txt' with different modes.

---
+++ Day3_Class_Programs/TC_File.py
@@ -0,0 +1,15 @@
+file=open("f1.txt","r")
+content=file.readline()
+content1=file.readlines()
+print(content)
+print(content1)
+file.close()
+
+file=open("f1.txt","a")
+file.write("\n New line added")
+file.close()
+
+file=open("f1.txt","w")
+file.write("Hello Python\n")
+file.write("This is my new write example")
+file.close()
