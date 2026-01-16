9930ab5410c60d791f2240df1d7f9a60618c0fee
BhagyashreeNBijap <bhagyan125@gmail.com>
2026-01-16 11:51:33 +0530

    Implement abstract class for Employee and Manager

---
+++ D4_Class_Programs/TC_ABC_Constructor.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class Employee(ABC):
+    def __init__(self,name):
+        self.name = name
+    @abstractmethod
+    def salary(self):
+        pass
+
+class Manager(Employee):
+    def salary(self):
+        print(self.name,"Salary is 50000")
+
+m = Manager("Bhagya")
+m.salary()
