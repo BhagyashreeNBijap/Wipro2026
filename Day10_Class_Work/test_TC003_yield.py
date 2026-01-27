bfb87d92be8892cd848ffac18f4aa225b308e3f5
BhagyashreeNBijap <bhagyan125@gmail.com>
2026-01-27 15:12:00 +0530

    File added

---
+++ Day10_Class_Work/test_TC003_yield.py
@@ -0,0 +1,19 @@
+import pytest
+
+@pytest.mark.smoke
+def test_smoke():
+    assert True
+
+@pytest.fixture()
+
+def setup_teardown():
+    print("setup")
+    yield
+    print("teardown")
+
+
+def test_example(setup_teardown):
+    print("test1 running")
+
+def test_example1(setup_teardown):
+    print("test2 running")
