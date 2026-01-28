499c060cf50e3f3651fb16b73f1ca9ddb1591af4
BhagyashreeNBijap <bhagyan125@gmail.com>
2026-01-29 00:48:04 +0530

    File added

---
+++ Day11_Class_Work/test_skip_xfail.py
@@ -0,0 +1,10 @@
+import sys
+import pytest
+
+@pytest.mark.skipif(sys.platform == "win32", reason="Not supported on Windows")
+def test_linux_only():
+    assert True
+
+@pytest.mark.xfail(reason="Known bug")
+def test_known_issue():
+    assert 2 * 2 == 5
