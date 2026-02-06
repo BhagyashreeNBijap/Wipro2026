3ac5d7d41a8f4caa9edc5d9e30fcaeb7d2f11004
BhagyashreeNBijap <bhagyan125@gmail.com>
2026-02-06 15:44:15 +0530

    File added

---
+++ Day19_Class_Work/TC002_JavaScriptExecutor.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+from selenium.webdriver.support.ui import Select
+driver=webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://www.amazon.in")
+#driver.execute_script("alert('Hello Amazon')")
+#driver.execute_script("window.scrollBy(0,900)")
+time.sleep(5)
+driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+driver.quit()
+
